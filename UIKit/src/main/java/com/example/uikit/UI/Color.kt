ee41f70fcd0ad1ff729475708e386b003a753c37
Sergey Lyadov <ly4dov.s@yandex.ru>
2026-01-31 20:01:28 +0300

    create colors

---
+++ UIKit/src/main/java/com/example/uikit/UI/Color.kt
@@ -0,0 +1,34 @@
+package com.example.uikit.UI
+
+import androidx.compose.runtime.snapshots.SnapshotApplyResult
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.Placeholder
+
+val Accent = Color(0xff00C07F)
+val AccentInactive = Color(0xff63D4B0)
+val Black = Color(0xff000000)
+val White = Color(0xffffffff)
+val Error = Color(0xffFD3535)
+val Success = Color(0xff00B712)
+val InputBg = Color(0xffF5F5F9)
+val InputStr = Color(0xffEBEBEB)
+val InputIcon = Color(0xffB8C1CC)
+val Placeholder = Color(0xff939396)
+val Description = Color(0xff7E7E9A)
+val CardStr = Color(0xffF4F4F4)
+
+val greenGradient = Brush.linearGradient(
+    colors = listOf(
+        Color(0xff75D8BC),
+        Color(0xff5BADCB)
+    )
+
+)
+val blueGradient = Brush.linearGradient(
+    colors = listOf(
+        Color(0xffB4D5FF),
+        Color(0xff60A6FD)
+    )
+)
+
